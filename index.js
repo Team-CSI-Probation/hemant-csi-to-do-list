337e067a4db9e5fcfd51e4bd4825080e51453a0d
Hemant Upadhyay <hemantupadhyay3012002@gmail.com>
2022-01-06 23:06:22 +0530

    JS started
    UI part almost completed, now onto JS

---
+++ index.js
@@ -0,0 +1,6 @@
+var todolist=document.getElementsByTagName("LI");
+
+for(var i=0; i < todolist.length; i++){
+    var span = document.createElement("SPAN");
+    var text = document.createTextNode()
+}
