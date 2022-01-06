7bce7ac020124c42e6513e1e396028529734ac37
Hemant Upadhyay <hemantupadhyay3012002@gmail.com>
2022-01-06 23:38:32 +0530

    JS
    addtask function
    (X) at the end of the list

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var todolist=document.getElementsByTagName("LI");
+function addtask(){
+    var listitem = document.createElement("li");
+    var textvalue = document.getElementById("task-to-do").value;
+    var task = document.createTextNode(textvalue);
+    listitem.appendChild(task);
+    if(textvalue === ''){
+        alert("Empty!");
+    }
 
-for(var i=0; i < todolist.length; i++){
+    else{
+        document.getElementById("mytask").appendChild(li);
+    }
+    document.getElementById("task-to-do").value = "";
+
     var span = document.createElement("SPAN");
-    var text = document.createTextNode()
+    var text = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(text);
+    li.appendChild(span);
+
+    for(var i = 0; i < close.length; i++){
+        close[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
